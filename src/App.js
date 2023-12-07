d8c2007f16ec6c84d5fcf2940857607c71618aa8
Ben Walker <benwalker1.personal@gmail.com>
2023-12-07 15:20:21 +1300

    working progress

--- src/App.js
+++ src/App.js
@@ -78,6 +78,30 @@ function App() {
       {successMessage && <p>{successMessage}</p>}
 
     </div>
+
+    // --------
+        // ------------------------- overviewContainer provides ability to position the helpRequestsContainer
+        <div className={Styles.OverviewContainer}>
+        {/* ------------------------ helpRequestsContainer holds all content displayed within the overContainer */}
+        <div className={Styles.StudentProfilesContainer}>
+          <div className={Styles.container}>
+            {users &&
+              users.map(function (user) {
+                return (
+                  <NavLink className={Styles.navLink} to={`/student/${user.student_id}`} key={user.student_id}>
+                    <div key={user.student_id} className={Styles.card}>
+                    <img src={user.profile_pic} alt="students" className={Styles.studentImage} />
+                    <p>{user.name.toUpperCase()}</p>
+                  </div>
+                  </NavLink>
+                );
+              })}
+          </div>
+        </div>
+        {/* ^^^^^^^^^^^^^^^ END of helpRequestsContainer */}
+      </div>
+      // ^^^^^^^^^^^^^^ END of overviewContainer
+    // ^^^^^^^
   );
 }
 
